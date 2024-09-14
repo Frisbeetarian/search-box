144df51fe32070be6bf7b840af769eeaac07a8d4
Frisbeetarian <mohamad.sleimanhaidar@gmail.com>
2024-09-14 12:01:38 +0300

    reintegrate articles found display

--- src/components/article-list.tsx
+++ src/components/article-list.tsx
@@ -1,13 +1,25 @@
 import { Article } from "@/components/article.tsx";
 import { ArticleListProps } from "@/lib/types.ts";
 import React from "react";
+import styles from "@/App.module.css";
 
 export const ArticleList: React.FC<ArticleListProps> = ({
   articles,
   searchQuery,
 }) => {
+  const filteredArticles = articles.filter(
+    (article) =>
+      article.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      article.content.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
   return (
     <div>
+      {searchQuery && (
+        <p className={styles.articlesCount}>
+          {filteredArticles.length} articles found
+        </p>
+      )}
       {articles.map((article) => (
         <Article
           key={article.id}
